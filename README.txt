bba950f97fdeef398077b797b0a6258ee0241919
ebin-antoo <ebin@ebin-antoo.com>
2015-01-29 14:38:59 -0500

    added pop

---
+++ README.txt
@@ -0,0 +1,7 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/anon/pen/XJamqG.
+
+ A bunch of different animation effects for the [Magnific Popup](http://dimsemenov.com/plugins/magnific-popup/)
+
+Forked from [Dmitry Semenov](http://codepen.io/dimsemenov/)'s Pen [Inline popup](http://codepen.io/dimsemenov/pen/GEKgb/).
+
+Forked from [Dmitry Semenov](http://codepen.io/dimsemenov/)'s Pen [Inline popup](http://codepen.io/dimsemenov/pen/GEKgb/).
---
+++ js/index.js
@@ -0,0 +1,46 @@
+// Inline popups
+$('#inline-popups').magnificPopup({
+  delegate: 'a',
+  removalDelay: 500, //delay removal by X to allow out-animation
+  callbacks: {
+    beforeOpen: function() {
+       this.st.mainClass = this.st.el.attr('data-effect');
+    }
+  },
+  midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+});
+
+
+$(function() {
+    $("#tabs").tabs();
+	});
+
+    $('div#about').perfectScrollbar({
+      suppressScrollX: true
+    });
+
+    $('div#services').perfectScrollbar({
+      suppressScrollY: true,
+      maxScrollbarLength: 50
+    });
+
+$('#tabs').tabs({
+    activate: function (event, ui) {
+        var activeTabId = $(this).tabs('option', 'active');
+        if(activeTabId == 0){
+        	$( ".top-my-services" ).fadeOut( "slow", function() {
+        		$(".top-about").show();	   
+        	});
+        }
+        else if(activeTabId == 1){
+        	$( ".top-about" ).fadeOut( "slow", function() {
+    			$(".top-my-services").show();
+  			});
+        }
+    }
+});
+
+$(function(){
+      $(".top-about").load("../pop/includes/top-about.html"),
+      $(".top-my-services").load("../pop/includes/top-my-services.html"); 
+    });
